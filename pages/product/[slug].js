2b95e6e73ddaedaa0ac54b8daf8dd8e797869783
johnn-oss <johnalphonsus3@gmail.com>
2023-04-24 04:41:42 +0100

    I added  the Cart page and its functionalities

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Layout from '../../components/Layout';
 import data from '../../utils/data';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Store } from '../../utils/Store';
 
 export default function ProductScreen() {
+  const { state, dispatch } = useContext(Store);
+  const router = useRouter();
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug === slug);
   if (!product) {
     return <div>Product Not Found</div>;
   }
+
+  const addToCartHandler = () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+
+    if (product.countInStock < quantity) {
+      alert('Sorry, Product is out of stock');
+      return;
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    router.push('/cart');
+  };
+
   return (
     <Layout title={product.name}>
       <div className="py-2">
@@ -50,7 +66,9 @@ export default function ProductScreen() {
             <div>Status</div>
             <div>{product.countInStock > 0 ? 'In stock' : 'Unavailable'}</div>
           </div>
-          <button className="primary-button w-full">Add to Cart</button>
+          <button className="primary-button w-full" onClick={addToCartHandler}>
+            Add to Cart
+          </button>
         </div>
       </div>
     </Layout>
