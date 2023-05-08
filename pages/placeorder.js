ac45f5aa09555407cd70306e624a9bad6adf6c18
johnn-oss <johnalphonsus3@gmail.com>
2023-05-08 20:13:48 +0100

    I modified the placeorder page

--- pages/placeorder.js
+++ pages/placeorder.js
@@ -122,9 +122,9 @@ export default function PlaceOrderScreen() {
                         </Link>
                       </td>
                       <td className=" p-5 text-right">{item.quantity}</td>
-                      <td className="p-5 text-right">${item.price}</td>
+                      <td className="p-5 text-right">&#8358;{item.price}</td>
                       <td className="p-5 text-right">
-                        ${item.quantity * item.price}
+                      &#8358;{item.quantity * item.price}
                       </td>
                     </tr>
                   ))}
@@ -142,25 +142,25 @@ export default function PlaceOrderScreen() {
                 <li>
                   <div className="mb-2 flex justify-between">
                     <div>Items</div>
-                    <div>${itemsPrice}</div>
+                    <div>&#8358;{itemsPrice}</div>
                   </div>
                 </li>
                 <li>
                   <div className="mb-2 flex justify-between">
                     <div>Tax</div>
-                    <div>${taxPrice}</div>
+                    <div>&#8358;{taxPrice}</div>
                   </div>
                 </li>
                 <li>
                   <div className="mb-2 flex justify-between">
                     <div>Shipping</div>
-                    <div>${shippingPrice}</div>
+                    <div>&#8358;{shippingPrice}</div>
                   </div>
                 </li>
                 <li>
                   <div className="mb-2 flex justify-between">
                     <div>Total</div>
-                    <div>${totalPrice}</div>
+                    <div>&#8358;{totalPrice}</div>
                   </div>
                 </li>
                 <li>
